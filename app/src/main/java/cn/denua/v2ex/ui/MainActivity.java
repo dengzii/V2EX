a877b7e64f61775707394c82690d72b53859404a
DENUA <denua@qq.com>
2018-10-13 22:26:10 +0800

    init project

---
+++ app/src/main/java/cn/denua/v2ex/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.denua.v2ex.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.denua.v2ex.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+
+
+    }
+}
---
+++ app/src/main/java/cn/denua/v2ex/ui/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package cn.denua.v2ex.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
+
