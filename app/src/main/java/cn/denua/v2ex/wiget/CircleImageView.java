13b450001f51120968f24bc61713b664a486d52f
DENUA <denua@qq.com>
2018-11-14 20:19:52 +0800

    Circle ImageView

--- app/src/main/java/cn/denua/v2ex/wiget/CircleImageView.java
+++ app/src/main/java/cn/denua/v2ex/wiget/CircleImageView.java
@@ -4,13 +4,20 @@
 
 package cn.denua.v2ex.wiget;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
-import android.graphics.PixelFormat;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
 
 /*
  * 圆形的 ImageView
@@ -18,27 +25,71 @@ import android.support.annotation.Nullable;
  * @author denua
  * @date 2018/10/31 12
  */
-public class CircleImageView extends Drawable {
+public class CircleImageView extends AppCompatImageView {
 
-    private Bitmap bitmap;
+    private float mWidth;
+    private float mHeight;
+    private float mRadius = 0.5f;
+    private Paint mPaint = new Paint();
+    private Matrix mMatrix;
 
-    @Override
-    public void draw(@NonNull Canvas canvas) {
+    public CircleImageView(Context context) {
+        this(context, null);
+    }
 
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mPaint.setAntiAlias(true);
+        mMatrix = new Matrix();
     }
 
     @Override
-    public void setAlpha(int alpha) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = getMeasuredWidth();
+        mHeight = getMeasuredHeight();
+        mRadius = Math.min(mWidth, mHeight)/2;
+    }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        Drawable drawable = getDrawable();
+        if ((drawable instanceof BitmapDrawable)) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            Bitmap b = getCircleBitmap(bitmap);
+            final Rect rectSrc  = new Rect(0, 0, b.getWidth(), b.getHeight());
+            final Rect rectDest = new Rect(0,0,    getWidth(),   getHeight());
+            mPaint.reset();
+            canvas.drawBitmap(b, rectSrc, rectDest, mPaint);
+        } else {
+            super.onDraw(canvas);
+        }
+    }
+
+    private Bitmap getCircleBitmap(Bitmap bitmap) {
+
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        mPaint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        int x = bitmap.getWidth();
+
+        canvas.drawCircle(x / 2, x / 2, x * mRadius, mPaint);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, mPaint);
+        return output;
     }
 
     @Override
     public void setColorFilter(@Nullable ColorFilter colorFilter) {
 
-    }
-
-    @Override
-    public int getOpacity() {
-        return PixelFormat.UNKNOWN;
     }
 }
