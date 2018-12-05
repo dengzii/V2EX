0feffdcc059acc053fe2b11356392dc6d7411d2d
DENUA <denua@qq.com>
2018-12-05 23:15:18 +0800

    所有节点,布局

---
+++ app/src/main/java/cn/denua/v2ex/adapter/NodeAdapter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2018 denua.
+ */
+
+package cn.denua.v2ex.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.denua.v2ex.model.Node;
+
+/*
+ * @author denua
+ * @date 2018/12/05 13
+ */
+public class NodeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<Node> mNodes;
+
+    public NodeAdapter(List<Node> nodes){
+        this.mNodes = nodes;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = new TextView(parent.getContext());
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        return new NodeViewHolder(view);
+    }
+
+    public void setNodes(List<Node> nodes){
+        this.mNodes = nodes;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((NodeViewHolder) holder).viewGroup.setText(mNodes.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNodes.size();
+    }
+
+    private static class NodeViewHolder extends RecyclerView.ViewHolder{
+
+        TextView viewGroup;
+        public NodeViewHolder(View itemView) {
+            super(itemView);
+            this.viewGroup = (TextView) itemView;
+        }
+    }
+}
--- app/src/main/java/cn/denua/v2ex/ui/AllNodeActivity.java
+++ app/src/main/java/cn/denua/v2ex/ui/AllNodeActivity.java
@@ -8,19 +8,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 import com.blankj.utilcode.util.ToastUtils;
 import com.orhanobut.logger.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.denua.v2ex.R;
+import cn.denua.v2ex.adapter.NodeAdapter;
 import cn.denua.v2ex.base.BaseNetworkActivity;
 import cn.denua.v2ex.interfaces.IResponsibleView;
 import cn.denua.v2ex.interfaces.ResponseListener;
 import cn.denua.v2ex.model.Node;
 import cn.denua.v2ex.service.NodeService;
+import cn.denua.v2ex.widget.LabelLaoutManager;
 
 /*
  * @author denua
@@ -28,12 +36,29 @@ import cn.denua.v2ex.service.NodeService;
  */
 public class AllNodeActivity extends BaseNetworkActivity implements ResponseListener<List<Node>> {
 
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.swipe_refresh_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private NodeAdapter mNodeAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_all_node);
         ButterKnife.bind(this);
 
+        mRecyclerView.setNestedScrollingEnabled(false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        layoutManager.setSmoothScrollbarEnabled(false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mNodeAdapter = new NodeAdapter(new ArrayList<>());
+        mRecyclerView.setAdapter(mNodeAdapter);
+        mSwipeRefreshLayout.setOnRefreshListener(this::onRefresh);
     }
 
     @Override
@@ -43,17 +68,36 @@ public class AllNodeActivity extends BaseNetworkActivity implements ResponseList
     }
 
     @Override
+    public void onStartRequest() {
+        super.onStartRequest();
+        mSwipeRefreshLayout.setRefreshing(true);
+    }
+
+    @Override
     public int getContextStatus() {
         return IResponsibleView.VIEW_STATUS_ACTIVATED;
     }
 
     @Override
+    public void onCompleteRequest() {
+        super.onCompleteRequest();
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
     public void onComplete(List<Node> result) {
+
+        mNodeAdapter.setNodes(result);
+        mNodeAdapter.notifyDataSetChanged();
         Logger.d(result.size());
     }
 
     @Override
     public void onFailed(String msg) {
         ToastUtils.showShort(msg);
+    }
+
+    public void onRefresh(){
+
     }
 }
