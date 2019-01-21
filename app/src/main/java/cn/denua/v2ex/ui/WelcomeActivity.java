3a2238b288df4b53fe413d3c8a28dc9d97972249
DENUA <denua@qq.com>
2019-01-21 23:30:25 +0800

    no title welcomeActivity

--- app/src/main/java/cn/denua/v2ex/ui/WelcomeActivity.java
+++ app/src/main/java/cn/denua/v2ex/ui/WelcomeActivity.java
@@ -1,17 +1,9 @@
 package cn.denua.v2ex.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
-import com.blankj.utilcode.util.ToastUtils;
-
-import cn.denua.v2ex.Config;
 import cn.denua.v2ex.R;
 import cn.denua.v2ex.base.BaseActivity;
-import cn.denua.v2ex.interfaces.ResponseListener;
-import cn.denua.v2ex.model.Account;
-import cn.denua.v2ex.service.UserService;
 
 public class WelcomeActivity extends BaseActivity{
 
